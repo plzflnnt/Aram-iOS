8c8095b03d96412a372d72727405d8cd0093b95b
plzflnnt <pzneto@hotmail.com>
2015-10-05 23:29:00 -0300

    Initial Commit

---
+++ Aram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Aram
+//
+//  Created by Paulo Neto on 05/10/15.
+//  Copyright © 2015 Zafalon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
